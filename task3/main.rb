e1c9472c6582cf4fb7c7c1061c2bcfcaa89538be
AsmarHajizada <ahajizada21639@ada.edu.az>
2024-10-22 23:19:17 +0400

    task3 todo update

--- task3/main.rb
+++ task3/main.rb
@@ -1,14 +1,16 @@
 # Library Catalog System
 
-# TODO: make genre and rating (maybe) common property for all items
+# TODO: make initialization better
 
 class LibraryItem
   attr_accessor :title, :creator, :pub_year
 
-  def initialize(title, creator, pub_year)  # creator is used instead of author to represent all subclasses
+  def initialize(title, creator, pub_year, genre=nil, rating=nil)  # creator is used instead of author to represent all subclasses
     @title = title
     @creator = creator
     @pub_year = pub_year
+    @genre = genre
+    @rating = rating
   end
 
   def old?
@@ -21,10 +23,8 @@ class Book < LibraryItem
   attr_accessor :num_pages
 
   def initialize(title, creator, pub_year, num_pages, genre=nil, rating=nil)  
-    super(title, creator, pub_year)
+    super(title, creator, pub_year, genre, rating)
     @num_pages = num_pages
-    @genre = genre
-    @rating = rating
   end
 
   def long_book?
@@ -54,10 +54,8 @@ class DVD < LibraryItem
   attr_accessor :duration, :director, :rating
 
   def initialize(title, creator, pub_year, genre=nil, duration=nil, rating=nil)
-    super(title, creator, pub_year)
-    @genre = genre
+    super(title, creator, pub_year, genre, rating)
     @duration = duration
-    @rating = rating
   end
 
   def high_rating?
@@ -88,11 +86,10 @@ end
 class CD < LibraryItem
   attr_accessor :duration, :artist, :num_tracks, :genre
 
-  def initialize(title, creator, pub_year, duration, num_tracks=nil, genre=nil)
-    super(title, creator, pub_year)
+  def initialize(title, creator, pub_year, duration=nil, num_tracks=nil, genre=nil, rating=nil)
+    super(title, creator, pub_year, genre, rating)
     @duration = duration
     @num_tracks = num_tracks
-    @genre = genre
   end
 
   def long_album?
@@ -103,12 +100,17 @@ class CD < LibraryItem
     puts "Title: #{@title}"
     puts "Artist: #{@creator}"
     puts "Publication Year: #{@pub_year}"
-    puts "Duration: #{@duration}"
+    if @duration
+      puts "Duration: #{@duration} minutes"
+    end
     if @num_tracks
       puts "Number of Tracks: #{@num_tracks}"
     end
     if @genre
       puts "Genre: #{@genre}"
+    end
+    if @rating
+      puts "Rating: #{@rating}"
     end
   end
 
@@ -120,3 +122,6 @@ puts item1.display_info
 item2 = Book.new("The Lightning Thief", "Rick Riordan", 2005, 377)
 item3 = Book.new("The Song of Ice and Fire", "George R. R. Martin", 1996, 694)
 puts item3.long_book?, item3.old?
+
+item4 = CD.new("idk", "Arctic Monkeys", 2018, duration=41, genre="Indie Rock", rating=4.5)
+puts item4.display_info
